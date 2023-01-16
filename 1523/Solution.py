c40e61dce74cfd4d57df5998f99ac13ac6de0641
hsnayrus <suryanshjain.1309@gmail.com>
2023-01-15 21:23:39 -0500

    Done

--- 1523/Solution.py
+++ 1523/Solution.py
@@ -1,3 +1,27 @@
+"""
+Given two non-negative integers low and high. Return the count of odd
+numbers between low and high (inclusive).
+
+ 
+
+Example 1:
+
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+Example 2:
+
+Input: low = 8, high = 10
+Output: 1
+Explanation: The odd numbers between 8 and 10 are [9].
+
+Leetcode Problem Number: 1523 
+"""
+
+
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        return 0
+        count = int((high - low) / 2)
+        if low % 2 == 1 or high % 2 == 1:
+            count = count + 1
+        return count
