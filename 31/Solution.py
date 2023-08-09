07ce79bb2870f35097930c03735c2f13b4b6473e
Hsnayrus <suryanshjain.1309@gmail.com>
2023-08-09 10:16:05 -0700

    Wrote the brute force solution, but leetcode doesn't accept it. Writing in C++ now

--- 31/Solution.py
+++ 31/Solution.py
@@ -34,3 +34,58 @@ Constraints:
 1 <= nums.length <= 100
 0 <= nums[i] <= 100
 """
+
+
+class Solution:
+    def recurPermutations(self,
+                          nums: List[int],
+                          all_permutations: List[List[int]],
+                          frequency_list: List[bool],
+                          current_permutation: List[int]):
+        if len(nums) == len(current_permutation):
+            all_permutations.append(current_permutation.copy())
+
+        for i in range(0, len(nums)):
+            if not frequency_list[i]:
+                current_permutation.append(nums[i])
+                frequency_list[i] = True
+                self.recurPermutations(
+                    nums, all_permutations, frequency_list, current_permutation)
+                current_permutation.pop(len(current_permutation) - 1)
+                frequency_list[i] = False
+
+    def findAllPermutations(self, nums: List[int]) -> List[List[int]]:
+        frequency_list = [False for i in range(len(nums))]
+        all_permutations = []
+        current_permutation = []
+        nums.sort()
+        self.recurPermutations(nums, all_permutations,
+                               frequency_list, current_permutation)
+        return all_permutations
+
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        """
+        Find all the permutations
+        Sort them if not already sorted(if the list is already sorted, then 
+        we don't need to sort the final list)
+        Find the next Permutation, if there is no next, return the first one
+        """
+        all_permutations = self.findAllPermutations(nums.copy())
+        i = 0
+        while True:
+            print("hello")
+            if nums == all_permutations[i]:
+                try:
+                    nums = all_permutations[i + 1]
+                except IndexError:
+                    nums = all_permutations[0]
+                break
+            i = i + 1
+
+
+if __name__ == "__main__":
+    s1 = Solution()
+    s1.nextPermutation([1, 2, 3])
