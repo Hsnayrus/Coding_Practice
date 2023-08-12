044b3fda6d42a50d4248eb63cf84fac993da67ec
Hsnayrus <suryanshjain.1309@gmail.com>
2023-08-11 17:54:57 -0700

    Wrote the optimal solution, leetcode throwing overflow error

--- 31/Solution.cpp
+++ 31/Solution.cpp
@@ -1,51 +1,41 @@
 #include <iostream>
+#include <limits>
 #include <stdlib.h>
 #include <vector>
 
+template <typename T> void printList(std::vector<T> list) {
+    for (size_t i = 0; i < list.size(); i++) {
+        std::cout << list[i] << " ";
+    }
+    std::cout << "\n";
+}
+
 class Solution {
-  private:
-    void recurPermute(std::vector<int> nums,
-                      std::vector<std::vector<int> >& allPermutations,
-                      std::vector<bool>& frequencyList,
-                      std::vector<int>& currentPermutation) {
-        if (nums.size() == currentPermutation.size()) {
-            allPermutations.push_back(currentPermutation);
-            return;
-        }
-        for (size_t i = 0; i < nums.size(); i++) {
-            if (frequencyList[i] != true) {
-                frequencyList[i] = true;
-                currentPermutation.push_back(nums[i]);
-                recurPermute(nums, allPermutations, frequencyList,
-                             currentPermutation);
-                currentPermutation.pop_back();
-                frequencyList[i] = false;
-            }
-        }
-    }
-    std::vector<std::vector<int> > findAllPermutations(std::vector<int> nums) {
-        std::vector<int> currentPermutation;
-
-        std::vector<bool> frequencyList;
-        for (size_t i = 0; i < nums.size(); i++) {
-            frequencyList.push_back(false);
-        }
-
-        std::vector<std::vector<int> > allPermutations;
-
-        recurPermute(nums, allPermutations, frequencyList, currentPermutation);
-        return allPermutations;
-    }
-
   public:
-    template <typename T> void printList(std::vector<T> list) {
-        for (size_t i = 0; i < list.size(); i++) {
-            std::cout << list[i] << " ";
-        }
-        std::cout << "\n";
-    }
     void nextPermutation(std::vector<int>& nums) {
-        std::next_permutation(nums.begin(), nums.end());
+        if (nums.size() < 3) {
+            std::reverse(nums.begin(), nums.end());
+        } else {
+            int breakpoint = INT_MIN;
+            for (size_t i = nums.size() - 2; i >= 0; i--) {
+                if (nums[i] < nums[i + 1]) {
+                    breakpoint = i;
+                    break;
+                }
+            }
+            if (breakpoint == INT_MIN) {
+                std::reverse(nums.begin(), nums.end());
+            } else {
+                for (size_t i = nums.size() - 1; i > breakpoint; i--) {
+                    if (nums[i] > nums[breakpoint]) {
+                        std::swap(nums[breakpoint], nums[i]);
+                        break;
+                    }
+                }
+                std::reverse(nums.begin() + breakpoint + 1, nums.end());
+            }
+            // Find an element greater than the one at breakpoint
+        }
     }
 };
 
@@ -55,9 +45,9 @@ int main() {
     nums.push_back(1);
     nums.push_back(2);
     nums.push_back(3);
-    s1.printList(nums);
+    printList(nums);
     std::cout << "\n";
     s1.nextPermutation(nums);
     std::cout << "\n";
-    s1.printList(nums);
+    printList(nums);
 }
