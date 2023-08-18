49526d47dc880df0071bf5fe697cf827c6ff6216
Hsnayrus <suryanshjain.1309@gmail.com>
2023-08-17 17:43:07 -0700

    Wrote a solution between better and brute force. Time Complexity should be O(n). Leetcode Submission Status: Runtime: 0ms, 100%ile Memory: 8.22mb, 41.08%ile

--- 75/Solution.cpp
+++ 75/Solution.cpp
@@ -35,10 +35,22 @@ nums[i] is either 0, 1, or 2.
 Follow up: Could you come up with a one-pass algorithm using only constant extra
 space?
 */
+template <typename T> void printList(std::vector<T> list) {
+    for (size_t i = 0; i < list.size(); i++) {
+        std::cout << list[i] << std::endl;
+    }
+    std::cout << std::endl;
+}
 class Solution {
   private:
     template <typename T>
-    void pushToFront(std::vector<T>& nums, std::vector, T element) {}
+    void pushToFront(std::vector<T>& nums, size_t insertIndex,
+                     size_t removeIndex, T element) {
+        typename std::vector<T>::iterator newIt;
+        newIt = nums.begin();
+        nums.erase(newIt + removeIndex);
+        nums.insert(newIt + insertIndex, element);
+    }
 
   public:
     void sortColors(std::vector<int>& nums) {
@@ -48,8 +60,34 @@ class Solution {
                 continue;
             } else if (nums[i] == 1) {
                 if (last0Index == SIZE_T_MAX) {
+                    pushToFront(nums, 0, i, nums[i]);
+                } else {
+                    pushToFront(nums, last0Index + 1, i, nums[i]);
+                }
+            } else {
+                pushToFront(nums, 0, i, nums[i]);
+                if (last0Index == SIZE_T_MAX) {
+                    last0Index = 0;
+                } else {
+                    last0Index += 1;
                 }
             }
         }
     }
 };
+
+int main() {
+    Solution s1;
+    std::vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(0);
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(1);
+    nums.push_back(0);
+    // nums.push_back(2);
+    // nums.push_back(0);
+    // nums.push_back(2);
+    s1.sortColors(nums);
+    printList(nums);
+}
