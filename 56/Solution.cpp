dea3a4fdd8adb0da6abe15703d7e7b5c849e83c0
Hsnayrus <suryanshjain.1309@gmail.com>
2023-08-22 18:42:08 -0700

    Wrote the brute force solution. Leetcode Submission Status: Runtime: 792ms, 5%ile Memory: 20.04mb, 14.46%ile

--- 56/Solution.cpp
+++ 56/Solution.cpp
@@ -31,5 +31,61 @@ intervals[i].length == 2
 class Solution {
   public:
     std::vector<std::vector<int> >
-    merge(std::vector<std::vector<int> >& intervals) {}
+    merge(std::vector<std::vector<int> >& intervals) {
+        std::vector<std::vector<int> > result;
+        std::sort(intervals.begin(), intervals.end());
+
+        for (size_t i = 0; i < intervals.size(); i++) {
+            std::vector<int> current(intervals[i].begin(), intervals[i].end());
+            std::cout << "Result Size: " << result.size() << std::endl;
+            if (result.size() != 0 && current[0] <= result.back()[1]) {
+                continue;
+            }
+            for (size_t j = 0; j < intervals.size(); j++) {
+                if (intervals[j][0] <= current[1]) {
+                    if (intervals[j][1] > current[1]) {
+                        current[1] = intervals[j][1];
+                    }
+                } else {
+                    break;
+                }
+            }
+            result.push_back(current);
+        }
+        return result;
+    }
 };
+
+template <typename T> void printList(const std::vector<T>& list) {
+    for (size_t i = 0; i < list.size(); i++) {
+        std::cout << list[i] << ", ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<std::vector<int> > intervals;
+    int first[] = {1, 4};
+    int second[] = {2, 6};
+    int third[] = {8, 10};
+    int fourth[] = {13, 14};
+    std::vector<int> firstV(first, (first + sizeof(first) / sizeof(int)));
+    std::vector<int> secondV(second, (second + sizeof(second) / sizeof(int)));
+    std::vector<int> thirdV(third, third + sizeof(third) / sizeof(int));
+    std::vector<int> fourthV(fourth, fourth + sizeof(fourth) / sizeof(int));
+    intervals.push_back(firstV);
+    intervals.push_back(secondV);
+    intervals.push_back(thirdV);
+    intervals.push_back(fourthV);
+    for (size_t i = 0; i < intervals.size(); i++) {
+        printList(intervals[i]);
+    }
+    std::cout << "Chala daalaa\n";
+    std::sort(intervals.begin(), intervals.end());
+    Solution s1;
+    std::vector<std::vector<int> > result = s1.merge(intervals);
+    for (size_t i = 0; i < intervals.size(); i++) {
+        printList(result[i]);
+    }
+    return EXIT_SUCCESS;
+}
