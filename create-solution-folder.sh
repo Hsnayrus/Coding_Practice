147adcaea701c90e8405e7af3cf12d28db8b956f
Hsnayrus <suryanshjain.1309@gmail.com>
2023-09-08 18:20:36 -0700

    Added interim requirements to the create-solution-folder.sh script

--- create-solution-folder.sh
+++ create-solution-folder.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Arguments needed are:
+# 1. Leetcode(-lc) or Hackerrank(-hr)
+# 2. Number of problem
+# 3. Title of problem(would be great if I could retrieve it myself
+# 4. Language in which I am planning to code said problem("flags or simple text. -cpp / 'cpp'")
+# 5. Extras:
+# 5a. Make code clean using software engineering principles
+# 5b. Check for all conditions of input
+
+# Script is supposed to:
+# 1. Create a solution folder, in the appropriate repository, based on the given command line arguments
+# 2. Create a git branch if one doesn't exist already, based on input received
+# 2a. Checkout that branch depending on what branch I am currently in
 
