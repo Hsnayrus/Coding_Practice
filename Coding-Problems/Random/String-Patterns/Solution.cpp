e6d6c6f418806dd9051fc0c0644705c489d61984
Hsnayrus <suryanshjain.1309@gmail.com>
2023-09-16 10:53:38 -0700

    Added the basic files along with the question in the  solution file

---
+++ Coding-Problems/Random/String-Patterns/Solution.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <stdlib.h>
+#include <vector>
+/*
+Given the length of a word(wordLen) and the maximum number of consecutive vowels
+that it can contain(maxVowels), determine how many unique words can be
+generated. Words will consist of English alphabetic letters a through z only.
+Vowels are v: {a, e, i, o, u}. Consonants are the remaining 21 letters. In the
+explanation, c and v represent consonants and vowels respectively.
+
+Explanation:
+
+wordLen = 1
+maxVowels = 1
+        Patterns: {v, c}
+        That means there are 26 possibilities, one for each letter in the
+alphabet.
+
+wordLen = 4
+maxVowels = 1
+        Patterns: {cccc, cccv, ccvc, cvcc, vccc, cvcv, vcvc, vccv}
+
+        (21 * 21 * 21 * 21) = 194481;
+        (21 * 21 * 21 * 5) + (21 * 21 * 5 * 21) + (21 * 5 * 21 * 21) +(5 * 21 *
+                21 * 21) = 185220;
+
+                (5 * 21 * 21 * 5) + (21 * 5 * 21 * 5) + (5 * 21 * 21 * 5) =
+11025
+ */
