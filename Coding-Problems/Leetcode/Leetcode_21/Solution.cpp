8a1879c3d8ac09fc07714c6bad634c3512515ea8
Hsnayrus <suryanshjain.1309@gmail.com>
2023-10-04 19:17:43 -0700

    Wrote a solution. Leetcode Submission status: Runtime: 7ms, 47.08%ile Memory: 15.49mb, 7.91%ile

--- Coding-Problems/Leetcode/Leetcode_21/Solution.cpp
+++ Coding-Problems/Leetcode/Leetcode_21/Solution.cpp
@@ -46,5 +46,64 @@ struct ListNode {
 
 class Solution {
   public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {}
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* newListHead = nullptr;
+        ListNode* newListTail = nullptr;
+        ListNode* tempList1 = list1;
+        ListNode* tempList2 = list2;
+        while (tempList1 != nullptr && tempList2 != nullptr) {
+            ListNode* temp = new ListNode();
+            if (tempList1->val <= tempList2->val) {
+                temp->val = tempList1->val;
+                tempList1 = tempList1->next;
+            } else {
+                temp->val = tempList2->val;
+                tempList2 = tempList2->next;
+            }
+            if (newListHead == nullptr) {
+                newListHead = temp;
+                newListTail = temp;
+            } else {
+                newListTail->next = temp;
+                newListTail = newListTail->next;
+            }
+        }
+        ListNode* remainingList = nullptr;
+        if (tempList1 == nullptr) {
+            remainingList = tempList2;
+        } else {
+            remainingList = tempList1;
+        }
+        while (remainingList != nullptr) {
+            if (newListHead == nullptr) {
+                newListHead = new ListNode(remainingList->val);
+                newListTail = newListHead;
+            } else {
+                newListTail->next = new ListNode(remainingList->val);
+                newListTail = newListTail->next;
+            }
+            remainingList = remainingList->next;
+        }
+        return newListHead;
+    }
 };
+
+int main() {
+    Solution s1;
+    ListNode* first3 = new ListNode(4);
+    ListNode* first2 = new ListNode(2, first3);
+    ListNode* first1 = new ListNode(1, first2);
+
+    ListNode* second3 = new ListNode(4);
+    ListNode* second2 = new ListNode(3, second3);
+    ListNode* second1 = new ListNode(1, second2);
+
+    ListNode* result = s1.mergeTwoLists(first1, second1);
+    ListNode* temp = result;
+    while (temp != nullptr) {
+        std::cout << temp->val << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+    return 0;
+}
