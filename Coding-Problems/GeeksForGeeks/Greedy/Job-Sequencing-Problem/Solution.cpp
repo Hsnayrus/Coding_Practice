ffd5b00b6fd9bcbd86724b4ccf70758c05511abe
Hsnayrus <suryanshjain.1309@gmail.com>
2023-10-12 18:24:48 -0700

    Wrote the solution on my own. GFG Submission Status: Total Time Taken: 1.65 Accuracy: 100% Testcases Passed: 215/215

--- Coding-Problems/GeeksForGeeks/Greedy/Job-Sequencing-Problem/Solution.cpp
+++ Coding-Problems/GeeksForGeeks/Greedy/Job-Sequencing-Problem/Solution.cpp
@@ -42,12 +42,87 @@ struct Job {
     int id;     // Job Id
     int dead;   // Deadline of job
     int profit; // Profit if job is over before or on deadline
+
+    Job(int init_id, int init_dead, int init_profit) {
+        id = init_id;
+        dead = init_dead;
+        profit = init_profit;
+    }
 };
 
+bool comparator(Job first, Job second) {
+    if (first.profit == second.profit) {
+        return first.id > second.id;
+    }
+    return first.profit > second.profit;
+}
+
 class Solution {
+  private:
+    int findHighestDeadline(std::vector<Job> & jobs) {
+        int highest = INT_MIN;
+        for (size_t i = 0; i < jobs.size(); i++) {
+            if (jobs[i].dead > highest) {
+                highest = jobs[i].dead;
+            }
+        }
+        // Will always return something other than INT_MIN
+        // if jobs is not empty
+        return highest;
+    }
+
+    bool insertIntoScheduledJobs(Job job, std::vector<int> & scheduledJobs) {
+        int insertIndex = INT_MIN;
+        for (int i = job.dead - 1; i >= 0; i--) {
+            if (scheduledJobs[i] == -1) {
+                insertIndex = i;
+                break;
+            }
+        }
+        if (insertIndex == INT_MIN) {
+            return false;
+        }
+        scheduledJobs[insertIndex] = job.profit;
+        return true;
+    }
+    void printArray(std::vector<int> array) {
+        for (size_t i = 0; i < array.size(); i++) {
+            std::cout << array[i];
+            if (i != array.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
   public:
     // Function to find the maximum profit and the number of jobs done.
     std::vector<int> JobScheduling(Job arr[], int n) {
-        // your code here
+        std::vector<Job> jobs;
+        // insert into jobs
+        for (int i = 0; i < n; i++) {
+            jobs.push_back(arr[i]);
+        }
+        std::vector<int> result(2, 0);
+        std::sort(jobs.begin(), jobs.end(), comparator);
+        int highest = findHighestDeadline(jobs);
+        std::vector<int> scheduledJobs(highest, -1);
+        for (size_t i = 0; i < jobs.size(); i++) {
+            bool insert = insertIntoScheduledJobs(jobs[i], scheduledJobs);
+            if (insert) {
+                result[0]++;
+                result[1] += jobs[i].profit;
+            }
+        }
+        return result;
     }
 };
+
+int main() {
+    Solution s1;
+    Job arr[] = {Job(1, 4, 20), Job(2, 1, 10), Job(3, 1, 40), Job(4, 1, 30)};
+    int n = 4;
+    std::vector<int> result = s1.JobScheduling(arr, n);
+    std::cout << result[0] << ", " << result[1] << std::endl;
+    return 0;
+}
