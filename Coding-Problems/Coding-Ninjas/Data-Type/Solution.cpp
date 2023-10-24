714cee47a3972d00041d2f2c640b4372926d46b8
hsnayrus <suryanshjain.1309@gmail.com>
2023-10-23 18:25:38 -0700

    Wrote the skeleton code for the problem

--- Coding-Problems/Coding-Ninjas/Data-Type/Solution.cpp
+++ Coding-Problems/Coding-Ninjas/Data-Type/Solution.cpp
@@ -4,5 +4,46 @@
 #include <stdlib.h>
 #include <vector>
 /*
+Problem Statement
 
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+
+
+Sample Input 1:
+Long
+
+
+Sample Output 1:
+8
+
+
+Explanation Of Sample Input 1 :
+The size of a Long variable is given as 8 bytes.
+
+
+Sample Input 2:
+Float
+
+
+Sample Output 2:
+4
+
+
+Explanation Of Sample Input 2 :
+The size of a Float variable is given as 4 bytes.
+
+
+Expected Time Complexity :
+The expected time complexity is O(1).
+
+
+Constraints :
+‘type’ is one of the data types given above.
+
+Time limit: 1 second
  */
+int dataTypes(string type) {
+    // Write your code here
+}
