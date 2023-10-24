aa13f3e046370ee6cbfaa59d92e99027ccb4de70
hsnayrus <suryanshjain.1309@gmail.com>
2023-10-23 18:01:01 -0700

    Wrote the skeleton code for the problem

---
+++ Coding-Problems/Coding-Ninjas/Find-Character-Case/cpp_basic_template/Solution.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <stdlib.h>
+#include <vector>
+/*
+Problem Statement
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+
+Sample Input 1 :
+v
+
+
+Sample Output 1 :
+0
+
+
+Explanation Of Sample Input 1:
+The input character is lowercase, so our answer is 0.
+
+
+Sample Input 2 :
+V
+
+
+Sample Output 2 :
+1
+
+
+Explanation Of Sample Input 2:
+The input character is uppercase, so our answer is 1.
+
+
+Sample Input 3 :
+#
+
+
+Sample Output 3 :
+-1
+
+
+Explanation Of Sample Input 3:
+The input character is not an alphabet, so our answer is -1.
+
+
+Constraints :
+The input can be any single character.
+ */
+
+int main() {
+    // Write your code here
+}
