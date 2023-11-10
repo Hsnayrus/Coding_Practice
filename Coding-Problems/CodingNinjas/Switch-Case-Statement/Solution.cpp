fa71d5fee20cc96ef1d073e8f1dbd0abba6910d6
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-10 10:54:16 -0800

    Wrote the solution. Submission Status: Testcases Passed: 50/50

--- Coding-Problems/CodingNinjas/Switch-Case-Statement/Solution.cpp
+++ Coding-Problems/CodingNinjas/Switch-Case-Statement/Solution.cpp
@@ -82,4 +82,14 @@ Time limit: 1 second
  */
 double areaSwitchCase(int ch, std::vector<double> a) {
     // Write your code here
+    switch (ch) {
+    case 1:
+        return M_PI * a[0] * a[0];
+        break;
+    case 2:
+        return a[0] * a[1];
+        break;
+    default:
+        return 0;
+    }
 }
