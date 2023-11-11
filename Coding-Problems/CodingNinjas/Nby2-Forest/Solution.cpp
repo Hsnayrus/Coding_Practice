c82e68f22226bdff136d2bac63633b145dae0b5b
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-10 18:53:36 -0800

    Wrote a more efficient solution. Submission Status: Runtime: 20ms, 99.47%ile Memory: 4273 kB, 70.15%ile Testcases Passed: 11/11

--- Coding-Problems/CodingNinjas/Nby2-Forest/Solution.cpp
+++ Coding-Problems/CodingNinjas/Nby2-Forest/Solution.cpp
@@ -45,7 +45,7 @@ Sample Output 2 :
 void nForest(int n) {
     // Write your code here.
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i + 1; j++) {
+        for (int j = 0; j <= i; j++) {
             std::cout << "* ";
         }
         std::cout << std::endl;
