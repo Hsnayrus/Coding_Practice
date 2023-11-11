439173f75a4fc93ad4982e48cecfc7846a3dcb35
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-10 19:17:58 -0800

    Wrote the skeleton code for the problem

---
+++ Coding-Problems/CodingNinjas/Seeding/Solution.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <stdlib.h>
+#include <vector>
+/*
+Problem Statement
+Sam is planting trees on the upper half region(separated by the left diagonal)
+of the square shared field. For every value of 'N', print the field if the trees
+are represented by "*".
+
+Example:
+Input: N = 3
+
+Output:
+* * *
+* *
+*
+
+Detailed Explanation:
+Input Format:
+The first and only line contains an Integer, 'N'
+Output Format:
+Print the pattern as specified
+
+Constraints:
+1 <= T <= 10
+1 <= N <= 25
+
+Sample Input 1:
+3
+Sample Output 1:
+* * *
+* *
+*
+Sample Input 2 :
+1
+Sample Output 2 :
+*
+Sample Input 3 :
+4
+Sample Output 3 :
+* * * *
+* * *
+* *
+*
+
+ */
+void seeding(int n) {
+    // Write your code here.
+}
