8bef62fc0964cb0648886ef347e3fb99d22cbb66
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-10 19:19:33 -0800

    Wrote the solution to the problem. Submission Status: Runtime: 22ms, 79.68%ile Memory: 4247kB, 70.06%ile Testcases Passed: 11/11

--- Coding-Problems/CodingNinjas/Seeding/Solution.cpp
+++ Coding-Problems/CodingNinjas/Seeding/Solution.cpp
@@ -48,4 +48,10 @@ Sample Output 3 :
  */
 void seeding(int n) {
     // Write your code here.
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
 }
