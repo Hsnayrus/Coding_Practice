4a9405bf5344d28c913dd83114f0968707130d82
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-10 18:42:52 -0800

    Wrote the solution. Submission Status: Runtime: 20ms, 99.59%ile Memory: 4222 kB, 99.57%ile Testcases Passed: 11/11

--- Coding-Problems/CodingNinjas/N-Forest/Solution.cpp
+++ Coding-Problems/CodingNinjas/N-Forest/Solution.cpp
@@ -36,5 +36,13 @@ Sample Output 3 :
 * * * *
 * * * *
 * * * *
-
  */
+void nForest(int n) {
+    // Write your code here.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+}
