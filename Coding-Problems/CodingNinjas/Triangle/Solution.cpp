b6f4ee3bcff67073e7b1bd04c9ca691d32200852
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-10 19:03:44 -0800

    Wrote the skeleton code for the problem

---
+++ Coding-Problems/CodingNinjas/Triangle/Solution.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <stdlib.h>
+#include <vector>
+/*
+Problem Statement
+Sam is making a Triangular painting for a maths project.
+An N-dimensional Triangle is represented by the lower triangle of the pattern
+filled with integers representing the row number. For every value of 'N', help
+sam to print the corresponding Triangle.
+
+Example:
+Input: N = 3
+
+Output:
+1
+2 2
+3 3 3
+
+Detailed Explanation:
+Input Format:
+The first and only line contains an integer, 'N'
+Output Format;
+Print the pattern as specified
+
+Sample Input 1:
+3
+Sample Output 1:
+1
+2 2
+3 3 3
+Sample Input 2 :
+1
+Sample Output 2 :
+1
+
+ */
+void triangle(int n) {
+    // Write your code here
+}
