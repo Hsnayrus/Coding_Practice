3ec35e983bf09ca281921de08239be36d76e6bd8
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-14 16:11:26 -0800

    Wrote the skeleton code for the problem

---
+++ Coding-Problems/CodingNinjas/Armstrong-Numbers/Solution.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <stdlib.h>
+#include <vector>
+/*
+Problem Statement
+You are given an integer 'n'.
+
+Return 'true' if 'n' is an Armstrong number and 'false' otherwise.
+
+Note:
+An armstrong number is a number(with 'k' digits) such that the sum of its digits
+raised to the 'kth' power is equal to the number itself. For Example, 371 is an
+armstrong number because 3^3 + 7^7 + 1^3 = 371.
+
+Example:
+Input: 'n' = 1634
+Output: True
+Explanation:
+1634 is an Amrstrong number as, 1^4 + 6^4 + 3^4 + 4^4 = 1634
+
+
+
+
+Sample Input 1 :
+1
+
+
+Sample Output 1 :
+true
+
+
+Explanation of Sample Input 1 :
+1 is an Armstrong number as, 1^1 = 1.
+
+
+Sample Input 2 :
+103
+
+
+Sample Output 2 :
+false
+
+
+Expected Time Complexity:
+Try to solve this in O(log(n)).
+
+
+Constraints:
+1 <= ‘n’ <= 10^9
+
+Time Limit: 1 sec
+
+ */
+bool checkArmstrong(int n) {
+    // Write your code here
+}
