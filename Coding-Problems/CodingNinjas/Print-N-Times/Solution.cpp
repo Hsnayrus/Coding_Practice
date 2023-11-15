4c5bc0ab577679babd59be7f1c2549be0cc987bf
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-15 12:02:46 -0800

    Wrote the optimal solution. Submission Status: Testcases Passed: 30/30

--- Coding-Problems/CodingNinjas/Print-N-Times/Solution.cpp
+++ Coding-Problems/CodingNinjas/Print-N-Times/Solution.cpp
@@ -54,6 +54,35 @@ Constraints:
 Time Limit: 1 sec
 
  */
+template <typename T> void print(std::vector<T> myVector) {
+    typename std::vector<T>::iterator myIterator = myVector.begin();
+    while (myIterator != myVector.end()) {
+        std::cout << *myIterator;
+        ++myIterator;
+        if (myIterator != myVector.end()) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+}
+void printNTimesWorker(int i, int n, std::vector<std::string>& result) {
+    if (i == n) {
+        return;
+    }
+    result.push_back("Coding Ninjas");
+    printNTimesWorker(i + 1, n, result);
+}
 std::vector<std::string> printNTimes(int n) {
     // Write your code here
+    int i = 0;
+    std::vector<std::string> result;
+    printNTimesWorker(i, n, result);
+    return result;
+}
+int main() {
+    int input;
+    std::cin >> input;
+    std::vector<std::string> result = printNTimes(input);
+    print(result);
+    return 0;
 }
