34f91b6ca410259afa2f19f112fa4e0f46f29cad
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-17 10:32:32 -0800

    Wrote an interative solution. Submission status: Testcases Passed: 50/50

--- Coding-Problems/CodingNinjas/Print-Fibonacci-Series/Solution.cpp
+++ Coding-Problems/CodingNinjas/Print-Fibonacci-Series/Solution.cpp
@@ -38,6 +38,35 @@ Constraints:
 Time Limit: 1 s
 
  */
+template <typename T> void printList(std::vector<T>& myVector) {
+    typename std::vector<T>::iterator myIterator = myVector.begin();
+    while (myIterator != myVector.end()) {
+        std::cout << *myIterator;
+        ++myIterator;
+        if (myIterator != myVector.end()) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+}
+
 std::vector<int> generateFibonacciNumbers(int n) {
     // Write your code here
+    std::vector<int> sequence;
+    sequence.push_back(0);
+    if (n == 1) {
+        return sequence;
+    }
+    sequence.push_back(1);
+    for (int i = 2; i < n; i++) {
+        sequence.push_back(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+}
+int main() {
+    int input;
+    std::cin >> input;
+    std::vector<int> result = generateFibonacciNumbers(input);
+    printList(result);
+    return 0;
 }
