3873cd4df97a17c427203c578b0d5cea7c225d5f
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-16 18:08:57 -0800

    Wrote the skeleton code for the problem

---
+++ Coding-Problems/CodingNinjas/Check-Palindrome-Recursive/Solution.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <utility>
+#include <vector>
+/*
+Problem Statement
+Determine if a given string 'S' is a palindrome using recursion. Return a
+boolean value of true if it is a palindrom and false if it is not.
+
+Note: You are not requierd to print anything, just implement the function.
+
+Example:
+
+Input:
+s = "racecar"
+
+Output:
+True
+
+Explanation:
+"racecar" is a palindrome
+
+Sample Input 1:
+abbba
+Sample Output 1:
+true
+Explanation Of Sample Input 1 :
+“abbba” is a palindrome
+Sample Input 2:
+abcd
+Sample Output 2:
+false
+Explanation Of Sample Input 2 :
+“abcd” is not a palindrome.
+Constraints:
+0 <= |S| <= 10^6
+where |S| represents the length of string S.
+
+ */
+bool isPalindrome(std::string& str) {
+    // Write your code here
+}
