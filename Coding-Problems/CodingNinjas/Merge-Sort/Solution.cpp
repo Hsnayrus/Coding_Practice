f57b3e619c178e483af2233bc8daa73c461e25fa
hsnayrus <suryanshjain.1309@gmail.com>
2023-11-21 22:40:09 -0800

    Wrote the skeleton code for the problem

---
+++ Coding-Problems/CodingNinjas/Merge-Sort/Solution.cpp
@@ -0,0 +1,42 @@
+#include "myHeader.hpp"
+/*
+Problem Statement
+You are given the starting 'I' and the ending 'r' positions of the array 'ARR'.
+You must sort the elements between 'i' and 'r'.
+
+Note:
+Change in the input array itself. So no need to return or print anything.
+
+Example:
+Input:
+'n' = 7
+'arr' = {2, 13, 4, 1, 3, 6, 28}
+
+Output:
+'arr' = {1, 2, 3, 4, 6, 13, 28}
+
+Explanation:
+After appying 'merge sort' on the input array, the output is {1, 2, 3, 4, 6, 13,
+28}
+
+Sample Input 1:>
+7
+2 13 4 1 3 6 28
+Sample Output 1:>
+1 2 3 4 6 13 28
+Explanation of Sample Output 1:>
+After applying 'merge sort' on the input array, the output is [1 2 3 4 6 13 28].
+Sample Input 2:>
+5
+9 3 6 2 0
+Sample Output 2:>
+0 2 3 6 9
+Explanation of Sample Output 2:>
+After applying 'merge sort' on the input array, the output is [0 2 3 6 9].
+Constraints :>
+1 <= N <= 10^3
+0 <= ARR[i] <= 10^9
+ */
+void mergeSort(int arr[], int l, int r) {
+    // Write Your Code Here
+}
