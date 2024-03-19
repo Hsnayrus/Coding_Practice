89aa8cb5d68d0dc89a07ea0b7bdf259e883ebbd3
hsnayrus <suryanshjain.1309@gmail.com>
2024-03-19 10:58:39 -0700

    Wrote the skeleton code for the problem

---
+++ Coding-Problems/Leetcode/253-Meeting-Rooms-2/Solution.cpp
@@ -0,0 +1,27 @@
+#include "myHeader.hpp"
+/*
+Given an array of meeting time intervals intervals where intervals[i] = [starti,
+endi], return the minimum number of conference rooms required.
+
+
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+
+
+Constraints:
+
+1 <= intervals.length <= 104
+0 <= starti < endi <= 106
+ */
+using std::vector;
+class Solution {
+  public:
+    int minMeetingRooms(vector<vector<int> >& intervals) {}
+};
