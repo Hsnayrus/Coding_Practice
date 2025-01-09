984204c05bb4695fff8c8a75257a3ccd9afd0d46
hsnayrus <suryanshjain.1309@gmail.com>
2025-01-09 15:00:03 -0800

    feat: Wrote a good solution for the problem

--- Coding-Problems/NeetCode/Valid-Palindrome/Solution.py
+++ Coding-Problems/NeetCode/Valid-Palindrome/Solution.py
@@ -26,7 +26,28 @@ Constraints:
     s is made up of only printable ASCII characters.
 
 """
+"was it a car or cat I saw"
 
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = s.strip().split()
+        s = ''.join(s).lower()
+        first = 0
+        second = len(s) - 1
+
+        while (first < second):
+            if not s[first].isalnum():
+                first += 1
+                continue
+            if not s[second].isalnum():
+                second -= 1
+                continue
+
+            if s[first] != s[second]:
+                return False
+
+            first += 1
+            second -= 1
+
+        return True
