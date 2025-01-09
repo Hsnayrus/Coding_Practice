2b2a224113603c46b4b5ea92db7b645dbb13f408
hsnayrus <suryanshjain.1309@gmail.com>
2025-01-08 20:24:27 -0800

    feat: Added a solutioin, REattempt

--- Coding-Problems/NeetCode/Contains-Duplicate/Solution.py
+++ Coding-Problems/NeetCode/Contains-Duplicate/Solution.py
@@ -19,3 +19,10 @@ Output: false
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
+        nums_set = set()
+        for num in nums:
+            if num in nums_set:
+                return True
+            nums_set.add(num)
+
+        return False
