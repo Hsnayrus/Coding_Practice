609161d97092974ffa7bb420248ed1b7025f5f89
hsnayrus <suryanshjain.1309@gmail.com>
2025-01-10 15:17:05 -0800

    feat: Wrote a solution, need to reattempt
    Wrote a solution. need to try a different on

--- Coding-Problems/Leetcode/541-Reverse-String-2/Solution.py
+++ Coding-Problems/Leetcode/541-Reverse-String-2/Solution.py
@@ -30,3 +30,8 @@ Constraints:
 
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i: i + k] = reversed(s[i:i+k])
+        s = "".join(s)
+        return s
