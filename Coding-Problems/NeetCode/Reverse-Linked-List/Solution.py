beec9e6b71eb1ff5a1a98c5ec162a08d8366a13d
hsnayrus <suryanshjain.1309@gmail.com>
2025-01-09 20:23:00 -0800

    feat: wrote the ideal solution

--- Coding-Problems/NeetCode/Reverse-Linked-List/Solution.py
+++ Coding-Problems/NeetCode/Reverse-Linked-List/Solution.py
@@ -31,3 +31,14 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current_node = head
+        previous_node = None
+        while current_node != None:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            if next_node == None:
+                break
+            else:
+                current_node = next_node
+        return current_node
