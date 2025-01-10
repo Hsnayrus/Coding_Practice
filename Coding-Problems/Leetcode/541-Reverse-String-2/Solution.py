cecc1428f530873575ed0582844aa0c519c7e133
hsnayrus <suryanshjain.1309@gmail.com>
2025-01-10 15:21:10 -0800

    feat: Wrote my own reverse function

--- Coding-Problems/Leetcode/541-Reverse-String-2/Solution.py
+++ Coding-Problems/Leetcode/541-Reverse-String-2/Solution.py
@@ -4,7 +4,7 @@ Given a string s and an integer k, reverse the first k characters for every 2k c
 
 If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
 
- 
+
 
 Example 1:
 
@@ -16,7 +16,7 @@ Example 2:
 Input: s = "abcd", k = 2
 Output: "bacd"
 
- 
+
 
 Constraints:
 
@@ -29,6 +29,15 @@ Constraints:
 
 
 class Solution:
+    def reverseList(self, s: list[str]) -> list[str]:
+        if len(s) < 2:
+            return s
+        first = 0
+        second = len(s) - 1
+        while (first < second):
+            s[first], s[second] = s[second], s[first]
+        return s
+
     def reverseStr(self, s: str, k: int) -> str:
         s = list(s)
         for i in range(0, len(s), 2*k):
