e7ccdfbda2dd434f80b44c2d1fee22dcc80219b3
hsnayrus <suryanshjain.1309@gmail.com>
2025-04-11 11:58:54 -0700

    feat: Wrote a solution
    Wrote a solution for this problem.
    Can be optimized further.
    String concatenation(+=) is very expensive in python as strings are
    immutable.
    A better solution would be to create a list of characters.

--- Coding-Problems/Leetcode/1768-Merge-Strings-Alternatively/Solution.py
+++ Coding-Problems/Leetcode/1768-Merge-Strings-Alternatively/Solution.py
@@ -39,6 +39,104 @@ word1 and word2 consist of lowercase English letters.
 
 """
 
+"""
+Algo Mk1:
+* Initialize i = 0
+* result = ""
+* while(true)
+* result += word1[i] 
+* result += word2[i]
+* i += 1
+* if i == len(word1) || i == len(word2):
+* break
+* If i == len(word1):
+* result += word2[i:-1]
+* else
+* result += word1[i: -1]
+* return result
+
+DR1 Mk1: 
+abcd
+pqrs
+i = 0
+result = ""
+
+result = a
+result = ap
+i = 1
+dont break
+
+result = apb
+result = apbq
+i = 2
+dont break
+
+result = apbqc
+result = apbqcr
+i = 3
+dont break
+
+result = apbqcrd
+result = qpbqcrds
+i = 4
+i == len(word1)
+break
+
+i == len(word1)
+result = apbqcrds + word[4:-1]
+
+DR2 Mk1:
+abc
+pq
+
+i = 0
+result = ""
+
+result = a
+result = ap
+i = 1
+dont break
+
+result = apb
+result = apbq
+i = 2
+break since i == len(word2) = 2
+2 != 3
+result = apbq + word1[2:-1] = apbq + c
+
+Algo Mk2:
+* Initialize i = 0
+* result = ""
+* while(true)
+* result += word1[i] 
+* result += word2[i]
+* i += 1
+* if i == len(word1) || i == len(word2):
+* break
+* If i == len(word1):
+* result += word2[i:]
+* else
+* result += word1[i:]
+* return result
+"""
+
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        if word1 == "":
+            return word2
+        if word2 == "":
+            return word1
+        i = 0
+        result = ""
+        while True:
+            result += word1[i]
+            result += word2[i]
+            i += 1
+            if i == len(word1) or i == len(word2):
+                break
+        if i == len(word1):
+            result += word2[i:]
+        else:
+            result += word1[i:]
+        return result
