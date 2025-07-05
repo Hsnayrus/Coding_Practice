23a362328f0c5f169eb718fb362d4fede027913a
hsnayrus <suryanshjain.1309@gmail.com>
2025-07-04 20:08:32 -0700

    feat: Singly Linked List Base Class and add_front
    Created the class for singly linked list, LinkedList, which
    has the head property, it's getter, setter, size it's getter,
    __len__ and __str__ along with the add_front(data) method.

---
+++ Data_Structures/LinkedList/singly_linked_list.py
@@ -0,0 +1,48 @@
+from typing import *
+
+
+class LinkedList():
+    class _Node():
+        def __init__(self, data, next=None):
+            self.data = data
+            self.next = next
+
+    def __init__(self, data=None):
+        self._head = self._Node(data=data)
+        self._size = 0
+
+    @property
+    def head(self):
+        return self._head
+
+    @head.setter
+    def head(self, new_head=None):
+        self._head = new_head
+
+    @property
+    def size(self):
+        return self._size
+
+    def add_front(self, data):
+        self._head = self._Node(data=data, next=self._head)
+        self._size += 1
+
+    def __len__(self):
+        return self._size
+
+    def __str__(self):
+        result = ""
+        temp = self._head
+        while temp is not None:
+            result += str(temp.data)
+            if temp.next is not None:
+                result += "\n"
+            temp = temp.next
+        return result
+
+
+if __name__ == "__main__":
+    my_list = LinkedList(5)
+    print(str(my_list))
+    my_list.add_front(4)
+    print(str(my_list))
